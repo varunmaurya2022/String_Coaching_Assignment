214becdb3e0faa57f8a308403c286753f2b86ed9
varun maurya <varunmaurya@2022>
2023-11-25 13:51:24 +0530

    Stringreverse files add

---
+++ Stringreverse.java
@@ -0,0 +1,12 @@
+public class Stringreverse {
+   public static void reverse(String s) {
+      char[] ch = s.toCharArray();
+      for (int i = ch.length - 1; i >= 0; i--) {
+         System.out.print(ch[i]);
+      }
+   }
+
+   public static void main(String... args) {
+      reverse("Hello");
+   }
+}
