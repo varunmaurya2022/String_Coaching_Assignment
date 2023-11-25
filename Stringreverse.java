ce2f9220f59ff3651e5b56c4a151a82af3099838
varun maurya <varunmaurya@2022>
2023-11-25 17:32:55 +0530

    add all changes

--- Stringreverse.java
+++ Stringreverse.java
@@ -1,12 +1,15 @@
 public class Stringreverse {
-   public static void reverse(String s) {
+   public static String reverse(String s) {
       char[] ch = s.toCharArray();
+      String s2 = "";
       for (int i = ch.length - 1; i >= 0; i--) {
-         System.out.print(ch[i]);
+         s2 += ch[i];
       }
+      return s2;
    }
 
    public static void main(String... args) {
-      reverse("Hello");
+      String s4 = reverse("Hello");
+      System.out.println(s4);
    }
 }
