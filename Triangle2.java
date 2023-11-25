fe7e112984f57c9711b622866a05bbc032bd97cc
varun maurya <varunmaurya@2022>
2023-11-25 18:12:06 +0530

    add all changes

--- Triangle2.java
+++ Triangle2.java
@@ -1,14 +1,15 @@
-public class Triangle2 {
+public class Traingle2 {
    public static void triangle2(String s) {
       for (int i = 0; i < s.length(); i++) {
-         for (int j = s.length(); j > i; j--) {
-            System.out.print(j + " ");
+         for (int j = 0; j <= s.length() - i - 1; j++) {
+            char c = s.charAt(j);
+            System.out.print(c + " ");
          }
          System.out.println("");
       }
    }
 
-   public static void main(String... args) {
+   public static void main(String... s) {
       triangle2("appsquadz");
    }
 }
